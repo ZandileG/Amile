ad0bec5d9adeed11b8c522896c2f1800bb74df07
ZandileG <2562617@students.wits.ac.za>
2025-08-19 15:17:15 +0200

    changes

--- src/Pages/Credits.jsx
+++ src/Pages/Credits.jsx
@@ -6,33 +6,21 @@ function Credits(){
     <section className="page">
       <h1 className="title">Credits</h1>
 
-    <h2 className="page-heading">Website Developed by</h2>
+    <h2 className="page-heading">Site Developed by</h2>
       <p className="page-list">Zandile Gebuza</p>
 
-    <h2 className="page-heading">Written, Illustrated & Narrated by</h2>
+    <h2 className="page-heading">Narrated & Illustrated by</h2>
       <p className="page-list">Zandile Gebuza</p>
 
-    <h2 className="page-heading">Music by</h2>
+    <h2 className="page-heading">Background Music by</h2>
 
-    <ul className="page-list">
-     {/*<li>Adventure (remaster) by Alexander Nakarada 
+    <section className="page-list">
+    <p>Adventure (remaster) by Alexander Nakarada 
       <a href="https://www.creatorchords.com" target="_blank" rel="noopener noreferrer">(https://www.creatorchords.com)</a>
       <br />Licensed under Creative Commons BY Attribution 4.0 License<br /> 
     <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener noreferrer">https://creativecommons.org/licenses/by/4.0/</a>
-    </li>
-
-   <li>Beyond the Horizon by Alexander Nakarada 
-      <a href="https://www.creatorchords.com" target="_blank" rel="noopener noreferrer">(https://www.creatorchords.com)</a>
-      <br />Licensed under Creative Commons BY Attribution 4.0 License<br />
-    <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener noreferrer">https://creativecommons.org/licenses/by/4.0/</a>
-    </li>
-
-    <li>Blue Hour by Alexander Nakarada 
-      <a href="https://www.creatorchords.com" target="_blank" rel="noopener noreferrer">(https://www.creatorchords.com)</a>
-      <br />Licensed under Creative Commons BY Attribution 4.0 License<br />
-    <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener noreferrer">https://creativecommons.org/licenses/by/4.0/</a>
-    </li>*/}
-    </ul>
+    </p>
+    </section>
     </section>
   );
 }
