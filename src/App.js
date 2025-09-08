4b6d9ca7a2fb04791c39da239cc11ab444ed3d03
ZandileG <2562617@students.wits.ac.za>
2025-09-08 13:47:33 +0200

    install button testing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Book from "./Components/Book";
 import "./App.css";
 import { MusicProvider } from "./Context/MusicContext";
+import InstallButton from "./Components/InstallButton";
 
 function App(){
   return(
     <main className="app-container">
       <MusicProvider>
       <Book />
+      <div style={{ marginTop: "20px", textAlign: "center" }}>
+          <InstallButton />
+        </div>
       </MusicProvider>
     </main>
   );
