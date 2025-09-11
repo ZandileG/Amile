4d4977f02ed3f59ca7d8a32dde68f78bffdcf024
ZandileG <2562617@students.wits.ac.za>
2025-09-11 15:39:09 +0200

    page 3 done

--- src/Pages/Page2.jsx
+++ src/Pages/Page2.jsx
@@ -7,21 +7,20 @@ import "../Styles/Page.css";
 function Page2(){
   const { currentTime, language, currentPage } = useContext(MusicContext);
   
-  const transcripts = {
-      en: [
-    { text: "One day, a curse fell upon Dumakude.", start: 0, end: 3 },
-    { text: "What once poured from the sky became a drizzle, then a whisper, then silence.", start: 4, end: 9 },
-    { text: "The roads were cracked and the trees stood like skeletons.", start: 10, end: 14 },
-    { text: "The hills were a dull brown and the river that used to hum", start: 15, end: 17 },
-    { text: "had dried into a narrow, muddy trail.", start: 18, end: 20 },
-     ],
+      const transcripts = {
+        en: [
+    { text: "In the heart of this beautiful, vibrant town, was a young girl named Amile.", start: 0, end: 5 },
+    { text: "Amile lived with her parents, Mandisa and Sinalo.", start: 6, end: 10 },
+    { text: "They named her Amile, a name with many meanings.", start: 11, end: 15 },
+    { text: "To them, it meant “still standing,”", start: 16, end: 18 },
+    { text: "a promise of the strength and resilience they saw within her.", start: 19, end: 22 },
+   ],
       zu: [
-    { text: "Ngelinye ilanga, isiqalekiso sawela kuDumakude.", start: 0, end: 3 },
-    { text: "Okwake kwatheleka esibhakabhakeni kwaba ukuconsa,", start: 4, end: 9 },
-    { text: "kwase kuba ukuhleba, kwathula kwathi cwaka.", start: 10, end: 14 },
-    { text: "Imigwaqo yayiqhekekile futhi izihlahla zazimile njengamathambo.", start: 15, end: 17 },
-    { text: "Amagquma ayensundu ngokufiphele futhi umfula owawuvame ukugeleza", start: 18, end: 20 },
-    { text: "wawusomile waba umzila omncane onodaka.", start: 21, end: 25 },
+    { text: "Kulelidolobha, kwakune ntombazanyana okuthiwa nguAmile.", start: 0, end: 5 },
+    { text: "UAmile wayehlala nabazali bakhe, uMandisa noSinalo.", start: 6, end: 10 },
+    { text: "Bamqamba uAmile, igama elinencazelo eziningi.", start: 11, end: 15 },
+    { text: "Kubo, kwakusho ukuthi “usamile,”", start: 16, end: 18 },
+    { text: "isithembiso samandla nokuqina ababekubona ngaphakathi kuye.", start: 19, end: 22 },
   ]
 };
     const transcript = transcripts[language];
@@ -33,7 +32,7 @@ function Page2(){
 
       <p className="page-text">
         {transcript.map((line, i) => {
-         const isActive = currentPage === 2 && currentTime >= line.start && currentTime <= line.end;
+        const isActive = currentPage === 2 && currentTime >= line.start && currentTime <= line.end;
           return(
             <span key={i} className={isActive ? "highlight" : ""}>
               {line.text + " "}
